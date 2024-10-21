d2591384328a8ba0238b159380616b4f7b5cd3a2
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-21 08:37:57 -0600

    Update RandomForests.py

--- RandomForests.py
+++ RandomForests.py
@@ -2,3 +2,15 @@
 # Fecha: 21 octubre 2024
 # Descripción: Random Forests.
 # GitHub: https://github.com/Jona163
+
+from DecisionTree import DecisionTree
+import numpy as np
+from collections import Counter
+
+class RandomForest:
+    def __init__(self, n_trees=10, max_depth=10, min_samples_split=2, n_feature=None):
+        self.n_trees = n_trees
+        self.max_depth=max_depth
+        self.min_samples_split=min_samples_split
+        self.n_features=n_feature
+        self.trees = []
