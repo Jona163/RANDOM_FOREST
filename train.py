55687e42dd1e2ef74d06a768de5565f1d164932f
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-21 08:40:54 -0600

    Update train.py

--- train.py
+++ train.py
@@ -20,3 +20,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
+
+clf = RandomForest(n_trees=20)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+acc =  accuracy(y_test, predictions)
+print(acc)
