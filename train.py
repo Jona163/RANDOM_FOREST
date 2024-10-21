5b24b25d84af2671b7bf6cb96c4b77a299f975d0
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-21 08:40:33 -0600

    Update train.py

--- train.py
+++ train.py
@@ -8,3 +8,11 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import numpy as np
 from RandomForest import RandomForest
+
+data = datasets.load_breast_cancer()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234
+)
