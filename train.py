9626b20a1e1c0b75d7e0e7e54a3eda7932696b2c
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-21 08:40:22 -0600

    Update train.py

--- train.py
+++ train.py
@@ -2,3 +2,9 @@
 # Fecha: 21 octubre 2024
 # Descripción: Random Forests.
 # GitHub: https://github.com/Jona163
+
+from random import random
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+from RandomForest import RandomForest
