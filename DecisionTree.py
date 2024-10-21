cdc9bd266b82b0a8131b6479c906b6a8142bda00
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-21 08:32:29 -0600

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -16,3 +16,18 @@ class Node:
         
     def is_leaf_node(self):
         return self.value is not None
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
+        self.min_samples_split=min_samples_split
+        self.max_depth=max_depth
+        self.n_features=n_features
+        self.root=None
+
+    def fit(self, X, y):
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1],self.n_features)
+        self.root = self._grow_tree(X, y)
+
+    def _grow_tree(self, X, y, depth=0):
+        n_samples, n_feats = X.shape
+        n_labels = len(np.unique(y))
